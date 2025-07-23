2ad459bf728e9d959540ff1a0a3a87c7ddef74d0
Joseph Myalla <chanangae@yandex.com>
2025-07-23 15:07:22 +0300

    updating nav bar and favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -65,13 +65,7 @@ const Header = () => {
 
        
         </nav>
-
-        <a
-          href="#signup"
-          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
-        >
-          New account
-        </a>
+      
         <Button className="hidden lg:flex" href="#login">
           Sign in
         </Button>
